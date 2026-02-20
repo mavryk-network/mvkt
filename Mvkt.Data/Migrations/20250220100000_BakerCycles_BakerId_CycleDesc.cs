070e0c67c2fdf1a0da13bc39bce98e491f2ecd7c
0xVoronov <johnvoronov@gmail.com>
2026-02-20 20:53:03 +0300

    Added migration with index

---
+++ Mvkt.Data/Migrations/20250220100000_BakerCycles_BakerId_CycleDesc.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Mvkt.Data;
+
+#nullable disable
+
+namespace Mvkt.Data.Migrations
+{
+    /// <inheritdoc />
+    [DbContext(typeof(MvktContext))]
+    [Migration("20250220100000_BakerCycles_BakerId_CycleDesc")]
+    public partial class BakerCycles_BakerId_CycleDesc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE INDEX IF NOT EXISTS ""IX_BakerCycles_BakerId_Cycle_Desc""
+                ON ""BakerCycles"" (""BakerId"", ""Cycle"" DESC);
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BakerCycles_BakerId_Cycle_Desc",
+                table: "BakerCycles");
+        }
+    }
+}
